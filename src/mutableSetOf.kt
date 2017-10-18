f4fe13087df0950816d57b2b668c5c4864c803c3
faidnawa <faidnawa@gmail.com>
2017-10-18 15:46:22 +0700

    ￼add setOf and mutableSetOf

---
+++ src/mutableSetOf.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by faidnawa on 18/10/17
+ */
+
+fun main(args: Array<String>) {
+    var setEmenetM= mutableSetOf(1,2,3,11,44,55,55)
+    setEmenetM.add(77)
+
+    for (element in setEmenetM){
+        println(element)
+    }
+
+}
---
+++ src/setOf.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by faidnawa on 18/10/17
+ */
+
+fun main(args: Array<String>) {
+    var setEmenet= setOf(1,2,3,11,44,55,55)
+    // setEmenet.add(77) not work
+    for (element in setEmenet){
+        println(element)
+    }
+}
