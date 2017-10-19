7f6a8d1d1d9ac53faa5261b068b0f67564463067
faidnawa <faidnawa@gmail.com>
2017-10-19 22:11:26 +0700

    add Global and Local Variable

---
+++ src/Global_and_Local_Variable.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by faidnawa on 19/10/17
+ */
+
+var fullname="faidl fahruddin nawa" // global var
+fun display():Unit{
+    println(fullname)
+}
+
+fun main(args: Array<String>) {
+    var name="faidl" //local var
+    println(fullname)
+    print("name:"+name)
+}
