7797a9897cc4a5e908e1061224f8032509d52f9e
faidnawa <faidnawa@gmail.com>
2017-10-24 11:43:24 +0700

    Constructor

---
+++ src/mobil4.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by SAINTEK 101 on 10/24/2017.
+ */
+class Mobil4(mesin:Int,roda:Int,body:Int){
+    init {
+        println("Mesin : $mesin")
+        println("Roda  : $roda")
+        println("Body  : $body")
+    }
+
+}
+
+fun main(args: Array<String>) {
+    Mobil4(1,4,1)
+}
